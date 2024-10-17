057c061fe9e68c16c80176231ad9ab6ff8c97dba
LuanGFRicardo <luanfranca.personaluser@gmail.com>
2024-10-17 20:34:22 -0300

    Comentários.

--- desafio_django/principal/views.py
+++ desafio_django/principal/views.py
@@ -5,19 +5,23 @@ import os
 
 # Create your views here.
 
+# View para página Home
 def home(request):
     return render(request, 'principal/home.html')
 
+# View para a página de inserção da URL da imagem
 def image_url(request):
     if request.method == 'POST':
         image_url = request.POST.get('image_url')
         return render(request, 'principal/image_url.html', {'image_url': image_url})
     return render(request, 'principal/image_url.html')
 
+# View para exibir a tabela de dados do CSV
 def exibir_tabela(request):
     dados = []
     caminho_csv = os.path.join(settings.BASE_DIR, 'alunos.csv')
 
+    # Lê o arquivo CSV
     with open(caminho_csv, newline='', encoding='utf-8') as csvfile:
         leitor_csv = csv.reader(csvfile)
         next(leitor_csv)
@@ -29,4 +33,5 @@ def exibir_tabela(request):
                 'altura': linha[3],
             })
 
+    # Renderiza a tabela com os dados extraídos do CSV
     return render(request, 'principal/exibir_tabela.html', {'dados': dados})
