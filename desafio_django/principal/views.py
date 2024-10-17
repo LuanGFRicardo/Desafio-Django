e1e44e212fa52ec1e34c248dfaa71a83e56721da
LuanGFRicardo <luanfranca.personaluser@gmail.com>
2024-10-17 19:57:55 -0300

    Template para Inserir e Exibir Imagem

--- desafio_django/principal/views.py
+++ desafio_django/principal/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'principal/home.html')
+
+def image_url(request):
+    if request.method == 'POST':
+        image_url = request.POST.get('image_url')
+        return render(request, 'principal/image_url.html', {'image_url': image_url})
+    return render(request, 'principal/image_url.html')
