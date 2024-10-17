7e403faafb4ceb38c914ab2d09a4ab7452e49db8
LuanGFRicardo <luanfranca.personaluser@gmail.com>
2024-10-17 20:21:45 -0300

    Template para Exibir a Tabela

--- desafio_django/principal/urls.py
+++ desafio_django/principal/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('image-url', views.image_url, name='image_url'),
+    path('exibir-tabela/', views.exibir_tabela, name='exibir_tabela'),
 ]
--- desafio_django/principal/views.py
+++ desafio_django/principal/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import csv
+from django.conf import settings
+import os
 
 # Create your views here.
 
@@ -10,3 +13,20 @@ def image_url(request):
         image_url = request.POST.get('image_url')
         return render(request, 'principal/image_url.html', {'image_url': image_url})
     return render(request, 'principal/image_url.html')
+
+def exibir_tabela(request):
+    dados = []
+    caminho_csv = os.path.join(settings.BASE_DIR, 'alunos.csv')
+
+    with open(caminho_csv, newline='', encoding='utf-8') as csvfile:
+        leitor_csv = csv.reader(csvfile)
+        next(leitor_csv)
+        for linha in leitor_csv:
+            dados.append({
+                'nome': linha[0],
+                'idade': linha[1],
+                'peso': linha[2],
+                'altura': linha[3],
+            })
+
+    return render(request, 'principal/exibir_tabela.html', {'dados': dados})
